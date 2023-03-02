cebfb9b04f67a04a06397526042b5688973c49bc
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-02 15:41:45 +0100

    Edit style into 'AppMain.vue' file

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,6 +20,7 @@ export default {
                 .then( (response) => {
                     console.log(response.data.results);
                     this.projects = response.data.results;
+                    console.log(this.projects);
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -44,12 +45,31 @@ export default {
             <div class="row">
 
                 <article class="col-4" v-for="project in projects">
-                    <div class="card">
+                    <div class="card mb-4">
                         <img :src="project.cover_image" class="card-img-top" :alt="project.title">
                         <div class="card-body">
-                            <h5 class="card-title">{{ project.title }}</h5>
-                            <p class="card-text">{{ project.content }}</p>
-                            <a href="#" class="btn btn-primary text-center">Show</a>
+                            <h5 class="card-title fw-bold text-center">{{ project.title }}</h5>
+                            <span class="card-text p-1 bg-success rounded-2 text-light mb-2">{{ project.type.type }}</span>
+                            <div class="technologies my-2">
+                                <span class="fw-bold">
+                                    Tecnologie utilizzate:
+                                </span>
+                                
+                                <p>
+                                    <ul>
+                                        <li v-for="technology in project.technologies">
+                                            {{ technology.name }}
+                                        </li>
+                                    </ul>
+                                </p>
+
+                            </div>
+                            
+                            <p class="card-text">{{ project.content.substr(0,200) }}...</p> 
+                            <div class="button text-center">
+                                <a href="#" class="btn btn-primary">Show more</a>
+                            </div>
+                            
                         </div>
                     </div>
                 </article>
