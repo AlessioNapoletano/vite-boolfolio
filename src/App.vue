c96005169e2543410ae1824450a48d7af56bd578
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-02 15:18:13 +0100

    Update file 'App.vue'

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    AppMain,
+  },
+
+  data() {
+    return {
+      
+    }
+  },
+
+}
 
 </script>
 
 <template>
+  <header>
+    <h1 class="text-center">
+      Projects API
+    </h1>
+  </header>
+  <main>
+    <AppMain />
+  </main>
   
 </template>
 
