ce077ea87d47d81ba7fe0227c3f3961ce250a305
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-06 16:14:18 +0100

    Update vue file 'SingleProject.vue'

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,11 +1,49 @@
 <script>
+import AppProject from '../components/AppProject.vue';
+import axios from 'axios';
+
 export default {
     name: 'SingleProject',
-}
+    components: {
+        AppProject
+
+    },
+
+    data() {
+        return {
+            project: null,
+            loading: false,
+            urlAddress: 'http://127.0.0.1:8000',
+        }
+    },
+
+    methods: {
+        getProject() {
+            axios.get(this.urlAddress + `/api/projects/${this.$route.params.slug}`, {
+                params: {}
+            })
+                .then((response) => {
+                    console.log('Qui siamo nel single page')
+                    this.project = response.data.results;
+                    console.warn(response);
+                })
+                .catch(function (error) {
+                    console.warn(error);
+                });
+        }
+    },
+
+    created() {
+    this.getProject();
+    },
+};
+
 </script>
 
 <template>
-
+    <div class="row">
+        <AppProject :project="project" :isShow="true" :imagePath="urlAddress" />
+    </div>
 </template>
 
 
